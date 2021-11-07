a04d61bad77ce0418ff805947d187a7dc2ec520c
Reginald <dono4executive@gmail.com>
2021-11-07 09:49:28 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Footer from './components/Footer';
+import Search from './components/Search';
+import './styles/App.css';
+
+function App() {
+    return (
+        <div className='container'>
+            <Search/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
